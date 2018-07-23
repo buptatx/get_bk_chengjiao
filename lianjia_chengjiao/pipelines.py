0c3dca77d3d8a419237fe09418df66ad7021045a
buptatx <buptatx@gmail.com>
2018-07-23 15:41:38 +0800

    add mysql process pipeline

--- lianjia_chengjiao/pipelines.py
+++ lianjia_chengjiao/pipelines.py
@@ -2,6 +2,7 @@
 
 import codecs
 import json
+import pymysql
 import time
 
 # Define your item pipelines here
@@ -22,3 +23,39 @@ class LianjiaChengjiaoPipeline(object):
 
     def spider_closed(self, spider):
         self.file.close()
+
+class LianjiaDBPipeline(object):
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host = settings.MYSQL_HOST,
+            port=3306,
+            db=settings.MYSQL_DBNAME,
+            user=settings.MYSQL_USER,
+            passwd=settings.MYSQL_PASSWD,
+            charset='utf8')
+
+        self.cursor = self.connect.cursor()
+
+    def process_item(self, item, spider):
+        try:
+            self.cursor.excute("select * from tbl_house_info where house_detail_link='%s'" 
+                % item["house_detail_link"])
+            rep = self.curcor.fetchone()
+        except Exception as error:
+            print("select error " + error)
+            self.cursor.close()
+            self.connect.close()
+            
+        if not rep:
+            try:
+                mexec = "insert into tbl_house_info () value`"
+                self.cursor.execute(mexec)
+                self.connect.commit()
+            except Exception as error:
+                print("insert error" + error)
+                self.cursor.close()
+                self.connect.close()
+
+        self.cursor.close()
+        self.connect.close()
+        return item
